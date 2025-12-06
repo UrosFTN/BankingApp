b37f01dd9016c79525fc6f8d534838b5962ee1b9
Uros Stanisavljevic <avangerex@gmail.com>
2025-12-06 15:30:15 +0100

    feat:AuthService -> Create auth service docker file and update docker-compose file

--- Backend/services/AuthService/src/config/server.config.ts
+++ Backend/services/AuthService/src/config/server.config.ts
@@ -1,5 +1,5 @@
 export const serverConfig = {
-  port: parseInt(process.env.PORT || "3001", 10),
+  port: parseInt(process.env.PORT || "50051", 10),
   host: "0.0.0.0",
   env: process.env.NODE_ENV || "development",
 };
