e4abaee7c196b03d525cc7f7ad0e63f3d283c814
Uros Stanisavljevic <avangerex@gmail.com>
2025-12-09 16:37:39 +0100

    Add LoginScreen

---
+++ Frontend/src/app/screens/auth/LoginScreen.tsx
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from "react-native";
+import { colors } from "../../../styles/colors";
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  async function handleForgotPassword() {
+    return;
+  }
+
+  async function handleLogin() {
+    return;
+  }
+
+  async function handleCreateAccount() {
+    return;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../../../../assets/logo.jpg")}
+        style={styles.hero}
+        resizeMode="contain"
+      />
+      <Text style={styles.title}>Login</Text>
+
+      <TextInput
+        placeholder="Email"
+        style={styles.input}
+        value={email}
+        onChangeText={setEmail}
+      />
+
+      <TextInput
+        placeholder="Password"
+        secureTextEntry
+        style={styles.input}
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      <TouchableOpacity onPress={handleForgotPassword} accessibilityRole="link">
+        <Text style={styles.forgotPassword}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={handleCreateAccount}>
+        <Text style={styles.buttonText}>Create Account</Text>
+      </TouchableOpacity>
+
+      {message ? <Text style={styles.message}>{message}</Text> : null}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.background,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: colors.whiteText,
+    outlineColor: colors.limeText,
+  },
+  input: {
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+    color: colors.whiteText,
+
+    backgroundColor: colors.foreground,
+    width: "80%",
+  },
+  message: { marginTop: 10, fontSize: 16 },
+  button: {
+    backgroundColor: colors.primaryButton,
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 5,
+    width: "80%",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  buttonText: {
+    color: colors.black,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  forgotPassword: {
+    color: colors.limeText,
+    textDecorationLine: "underline",
+    marginBottom: 12,
+  },
+  hero: {
+    width: 180,
+    height: 140,
+    marginBottom: 16,
+  },
+});
---
+++ Frontend/src/styles/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  primaryButton: "#b1eb34",
+
+  whiteText: "#FFFFFF",
+  limeText: "#b1eb34",
+
+  foreground: "#182e1a",
+  background: "#050a05ff",
+
+  black: "#000000",
+};
