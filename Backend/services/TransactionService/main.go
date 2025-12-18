4545633b0f0c1ce502bb0eaafe1d856249ce4699
Uros Stanisavljevic <avangerex@gmail.com>
2025-12-18 13:25:59 +0100

    feat: TransactionService -> Add main

--- Backend/services/TransactionService/main.go
+++ Backend/services/TransactionService/main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+	"transaction-service/config"
+	"transaction-service/server"
+
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetLevel(logrus.InfoLevel)
+
+	logrus.Info("Starting Transaction Service...")
+
+	if err := config.ConnectDatabase(); err != nil {
+		logrus.WithError(err).Fatal("Failed to connect to database")
+	}
+	defer config.CloseDatabase()
+
+	srv := server.NewServer()
+	go func() {
+		if err := srv.Start(); err != nil {
+			logrus.WithError(err).Fatal("Failed to start gRPC server")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	logrus.Info("Shutting down Transaction Service...")
+	srv.Stop()
+	logrus.Info("Transaction Service stopped")
+}
